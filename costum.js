449ce0771d508984cf26762b6cb88b028379dcb3
Saurabh <srivasaurabh79@gmail.com>
2021-06-10 04:30:03 +0530

    Initial Commit

--- costum.js
+++ costum.js
@@ -1,41 +1,180 @@
-// This demo uses the Chartjs javascript library
-// Simple yet flexible JavaScript charting for designers & developers
-// Webite: https://www.chartjs.org
-// CDN: https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js
-
-const percent = 84;
-const color = '#01713c';
-const canvas = 'chartCanvas';
-const container = 'chartContainer';
-
-const percentValue = percent; // Sets the single percentage value
-const colorGreen = color, // Sets the chart color
-    animationTime = '1400'; // Sets speed/duration of the animation
-
-const chartCanvas = document.getElementById(canvas), // Sets canvas element by ID
-    chartContainer = document.getElementById(container), // Sets container element ID
-    divElement = document.createElement('div'), // Create element to hold and show percentage value in the center on the chart
-    domString = '<div class="chart__value"><p>' + percentValue + '%</p></div>'; // String holding markup for above created element
-
-// Create a new Chart object
-const doughnutChart = new Chart(chartCanvas, {
-    type: 'doughnut', // Set the chart to be a doughnut chart type
-    data: {
-        datasets: [
-            {
-                data: [20, 80], // Set the value shown in the chart as a percentage (out of 100)
-                backgroundColor: ['white','red'], // The background color of the filled chart
-                borderWidth: 0 // Width of border around the chart
-            }
-        ]
+new Chart(document.getElementById("dougnt-1"), {
+  type: "doughnut",
+  data: {
+    labels: ["HTML", "CSS", "JS"],
+    datasets: [
+      {
+        backgroundColor: [
+          "rgb(255,0,0)",
+          "rgb(229,229,229)",
+          "rgb(255,255,255)",
+        ],
+        data: [70, 20, 10],
+      },
+    ],
+  },
+  options: {
+    responsive: false,
+    plugins: {
+      legend: {
+        display: false,
+        labels: {
+          color: "rgb(255, 99, 132)",
+        },
+      },
     },
-    options: {
-        cutoutPercentage: 84, // The percentage of the middle cut out of the chart
-        responsive: false, // Set the chart to not be responsive
-        tooltips: {
-            enabled: false // Hide tooltips
-        }
-    }
+    rotation: 188,
+  },
 });
 
- 
+new Chart(document.getElementById("dougnt-2"), {
+  type: "doughnut",
+  data: {
+    labels: ["HTML", "CSS", "JS"],
+    datasets: [
+      {
+        backgroundColor: [
+          "rgb(255,0,0)",
+          "rgb(229,229,229)",
+          "rgb(255,255,255)",
+        ],
+        data: [60, 30, 10],
+      },
+    ],
+  },
+  options: {
+    responsive: false,
+    plugins: {
+      legend: {
+        display: false,
+        labels: {
+          color: "rgb(255, 99, 132)",
+        },
+      },
+    },
+    rotation: 180,
+  },
+});
+
+new Chart(document.getElementById("line-chart1"), {
+  type: "line",
+  data: {
+    labels: ["9am", "1am", "5am", "9pm", "1am", "5am"],
+    datasets: [
+      {
+        data: [65, 59, 80, 81, 56, 55],
+        borderColor: "rgb(73,152,194)",
+        fill: false,
+        tension: 0.2,
+      },
+    ],
+  },
+  options: {
+    // responsive: false,
+    title: {
+      display: true,
+      text: "World population per region (in millions)",
+    },
+
+    plugins: {
+      legend: {
+        display: false,
+        labels: {
+          color: "rgb(255, 99, 132)",
+        },
+      },
+    },
+    scales: {
+      y: {
+        grid: {
+          display: false,
+        },
+        ticks: {
+          callback: function (value, index, values) {
+            return value + " A";
+          },
+        },
+      },
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+    },
+  },
+});
+
+new Chart(document.getElementById("line-2-1st"), {
+  type: "line",
+  data: {
+    labels: ["9am", "1am", "5am", "9pm", "1am", "5am"],
+    datasets: [
+      {
+        data: [50, 45, 80, 60, 90, 40],
+        borderColor: "rgb(73,152,194)",
+        fill: false,
+        tension: 0.5,
+      },
+    ],
+  },
+  options: {
+    // responsive: false,
+    title: {
+      display: true,
+      text: "World population per region (in millions)",
+    },
+
+    plugins: {
+      legend: {
+        display: false,
+        labels: {
+          color: "rgb(255, 99, 132)",
+        },
+      },
+    },
+    scales: {
+      y: {
+        grid: {
+          display: false,
+        },
+        ticks: {
+          callback: function (value, index, values) {
+            return value + " C";
+          },
+        },
+      },
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+    },
+  },
+});
+
+new Chart(document.getElementById("bar"), {
+  type: "bar",
+  data: {
+    labels: ["9am", "1am", "5am", "9pm", "1am", "5am"],
+    datasets: [
+      {
+        label: "Avg Current",
+        backgroundColor: "#4F98C3",
+        data: [50, 45, 80, 60, 90, 40],
+      },
+      {
+        label: "Temprature",
+        backgroundColor: "rgb(255,0,0)",
+        data: [50, 45, 80, 60, 90, 40],
+      },
+    ],
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Population growth (millions)",
+    },
+
+    // responsive: false,
+  },
+});
