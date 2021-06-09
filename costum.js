11b7f45f35a492af7ca615cb0c8de68289d8b551
Saurabh <srivasaurabh79@gmail.com>
2021-06-09 19:21:18 +0530

    Some change in style

--- costum.js
+++ costum.js
@@ -1,37 +1,41 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'doughnut',
+// This demo uses the Chartjs javascript library
+// Simple yet flexible JavaScript charting for designers & developers
+// Webite: https://www.chartjs.org
+// CDN: https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js
+
+const percent = 84;
+const color = '#01713c';
+const canvas = 'chartCanvas';
+const container = 'chartContainer';
+
+const percentValue = percent; // Sets the single percentage value
+const colorGreen = color, // Sets the chart color
+    animationTime = '1400'; // Sets speed/duration of the animation
+
+const chartCanvas = document.getElementById(canvas), // Sets canvas element by ID
+    chartContainer = document.getElementById(container), // Sets container element ID
+    divElement = document.createElement('div'), // Create element to hold and show percentage value in the center on the chart
+    domString = '<div class="chart__value"><p>' + percentValue + '%</p></div>'; // String holding markup for above created element
+
+// Create a new Chart object
+const doughnutChart = new Chart(chartCanvas, {
+    type: 'doughnut', // Set the chart to be a doughnut chart type
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+        datasets: [
+            {
+                data: [20, 80], // Set the value shown in the chart as a percentage (out of 100)
+                backgroundColor: ['white','red'], // The background color of the filled chart
+                borderWidth: 0 // Width of border around the chart
+            }
+        ]
     },
     options: {
-        responsive:false,
-        scales: {
-            
-        },
-        legend:{
-            display:false
+        cutoutPercentage: 84, // The percentage of the middle cut out of the chart
+        responsive: false, // Set the chart to not be responsive
+        tooltips: {
+            enabled: false // Hide tooltips
         }
     }
 });
+
+ 
